e13192177fbb96952a9cfd68d9e12ee6b47110b5
Willians Patino <ppwj@yahoo.com>
2026-03-29 13:38:39 -0400

    Encript SSH key script

---
+++ bin/executable_encrypt-ssh-keys.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -eu
+
+src_dir="$HOME/.ssh"
+dst_dir="$HOME/.config/chezmoi"
+
+mkdir -p "$dst_dir"
+
+# Identifica tu clave GPG (ajusta el correo)
+RECIPIENT="ppwj@yahoo.com"
+
+echo "Encriptando id_ed25519..."
+gpg --armor --encrypt \
+  --recipient "$RECIPIENT" \
+  "$src_dir/id_ed25519"
+
+# Mueve a la carpeta de chezmoi con nombre estándar
+mv "$src_dir/id_ed25519.asc" "$dst_dir/encrypted_private_id_ed25519.asc"
+
+echo "Encriptando id_rsa..."
+gpg --armor --encrypt \
+  --recipient "$RECIPIENT" \
+  "$src_dir/id_rsa"
+
+mv "$src_dir/id_rsa.asc" "$dst_dir/encrypted_private_id_rsa.asc"
+
+echo "Claves encriptadas guardadas en:"
+echo "  $dst_dir/encrypted_private_id_ed25519.asc"
+echo "  $dst_dir/encrypted_private_id_rsa.asc"
+
+# Añade a chezmoi (opcional, pero recomendado)
+chezmoi add "$dst_dir/encrypted_private_id_ed25519.asc"
+chezmoi add "$dst_dir/encrypted_private_id_rsa.asc"
+
+echo "Done. Run 'chezmoi cd && git commit -m \"Update encrypted SSH keys\" && git push' when ready."
+
